dc274942fa5eea5cfe4b772d72251758081f9cfb
Aleksandr Sergeev <alexx.sergeev@gmail.com>
2020-12-29 23:54:05 +0500

    created recipe_card, profile templates

--- foodgram_project/recipes/views.py
+++ foodgram_project/recipes/views.py
@@ -5,28 +5,47 @@ from django.views.generic import ListView, DetailView, CreateView
 
 from recipes.forms import RecipeForm
 from recipes.models import Recipe, Tag
+from users.models import User
 
 
 def recipe_list(request):
-    recipe_list = Recipe.objects.filter(draft=False)
-    paginator = Paginator(recipe_list, 3)
+    recipes = Recipe.objects.filter(draft=False)
+    paginator = Paginator(recipes, 3)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    return render(request, 'index.html', {'page': page, 'paginator': paginator, 'index': True})
+
+    return render(
+        request,
+        'index.html',
+        {
+            'page': page,
+            'paginator': paginator,
+            'index': True
+        }
+    )
 
 
 def tag_recipes(request, slug):
     tags = get_object_or_404(Tag, slug=slug)
-    recipe_list = Recipe.objects.filter(tags=tags)
-    paginator = Paginator(recipe_list, 3)
+    recipes = Recipe.objects.filter(tags=tags)
+    paginator = Paginator(recipes, 3)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
-    return render(request, 'index.html', {'page': page, 'paginator': paginator,'index': True})
+
+    return render(
+        request,
+        'index.html',
+        {
+            'page': page,
+            'paginator': paginator,
+            'index': True
+        }
+    )
 
 
 def recipe_detail(request, slug):
     recipe = get_object_or_404(Recipe, slug=slug)
-    return render(request, 'recipes/singlePage.html', {'recipe': recipe})
+    return render(request, 'recipes/recipe.html', {'recipe': recipe})
 
 
 @login_required
@@ -37,7 +56,26 @@ def new_recipe(request):
         new_recipe.author = request.user
         new_recipe.save()
         return redirect('home')
-    return render(request, 'recipes/formRecipe.html', {'form': form})
+    return render(request, 'recipes/new_recipe.html', {'form': form})
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    recipes = author.recipes.all()
+    paginator = Paginator(recipes, 3)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'recipes/profile.html',
+        {
+            'paginator': paginator,
+            'page': page,
+            'author': author,
+            'index': True
+        }
+    )
 
 
 # class RecipeListView(ListView):
