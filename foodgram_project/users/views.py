c950172df547d95c694ddd450232719fb7008218
Aleksandr Sergeev <alexx.sergeev@gmail.com>
2021-01-04 13:22:52 +0500

    add reset password and change password templates

--- foodgram_project/users/views.py
+++ foodgram_project/users/views.py
@@ -8,4 +8,4 @@ from users.forms import CreationForm
 class SignUp(CreateView):
     form_class = CreationForm
     success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+    template_name = 'signup.html'
